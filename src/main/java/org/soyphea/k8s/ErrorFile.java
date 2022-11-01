ab3e329c9985a14b44e2aad0e3d9b7effd711ba3
Sebastián Madrid <sebastiane.madrid@gmail.com>
2022-11-01 14:19:52 -0300

    Create ErrorFile.java

---
+++ src/main/java/org/soyphea/k8s/ErrorFile.java
@@ -0,0 +1,26 @@
+package com.ecodeup.errores;
+
+public class TestErrores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// errores en tiempo de copilación
+		int suma=0;
+		String nombre;
+    String password:
+    password='12345':
+		suma=nombre; //error 1
+		
+		int valor=0;
+		short numero=0;
+		numero=valor; //error 2
+		
+		String valor=""; //error 3
+		
+		int mayor;
+        int menor;
+		if (true){
+			mayor=menor;//error 4
+		}
+	}
+}
