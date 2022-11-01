91d35bb529389df78586c478ebfa59c5e03c752c
Sebastián Madrid <sebastiane.madrid@gmail.com>
2022-11-01 14:24:34 -0300

    Update ErrorFile.java

--- src/main/java/org/soyphea/k8s/ErrorFile.java
+++ src/main/java/org/soyphea/k8s/ErrorFile.java
@@ -1,19 +1,5 @@
-package org.soyphea.k8s.srevice;
-
-import org.soyphea.k8s.domain.User;
-import org.springframework.stereotype.Service;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-@Service
-public class UserServiceError {
-
-    List<User> users = Arrays.asList(new User(1, "Dara"), new User(2, "Seyha"));
-
-    public List<User> getUser(String containName) {
-
-        return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
-    }
-
-}
+int numero = 2;
+String cadena = "Hola";
+String password = "12345";
+long decimal = 2.4;
+boolean flag = true;
